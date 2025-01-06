3e8030de46f524e461d77f1a8d39122bcd2bce43
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-05 21:49:03 -0600

    add direction enum

---
+++ source_code/direction.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace App {enum struct Direction : std::uint_least8_t {
+	/**/          north=1,
+	/**/
+	/**/ west=4,  none=0,   east=2,
+	/**/
+	/**/          south=3,
+};}

077fc463660f29363b089c40dcd22194bfb0b398
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-06 02:30:59 -0600

    string canvas

---
+++ source_code/string_canvas.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string_view>
+
+namespace App { static constexpr std::string_view stringCanvas{
+R"(
+                                                                              
+       xxxxxx        xxxxxxxx      xxxxxxxx         xxxxxx      xx            
+     xx     xx      xx      xxx    x       xx     xxx    xxx    xx            
+    xx       x      x         x    x     xxxx    xx        x    xx            
+    x              xx         x    xxxxxxxxx     x         x    xx            
+    x              xx         x    x       xx    x         x    xx            
+    x        x      x        xx    x        x    x         x    xx            
+    xx      xx      xx      xx     x      xxx    xx      xxx    xxxxxxxxxx    
+     xxxxxxxx        xxxxxxx       xxxxxxxx       xxxxxxx       xxxxxxxxxx    
+                                                                              
+)"
+};}

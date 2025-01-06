77944e9a9e3e3f998cf8b46f0ef3f068191f852b
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-06 02:47:56 -0600

    make from string canvas

---
+++ source_code/make_from_string_canvas.cpp
@@ -0,0 +1,38 @@
+#include "cell_grid.hpp"
+#include "string_canvas.hpp"
+
+#include <algorithm>
+#include <random>
+
+auto App::CellGrid::makeFromStringCanvas() -> CellGrid {
+	CellGrid grid(10, 78);
+
+	CellGridKey currentLocation{0, 0};
+
+	std::vector<CellGridKey> workerTargetLocations;
+
+	for (
+		auto valueIterator = stringCanvas.cbegin() + 1;
+		valueIterator != stringCanvas.cend();
+		++valueIterator
+	) {
+		char const value{*valueIterator};
+		switch (value) {
+			case 'x':
+				workerTargetLocations.push_back(currentLocation);
+				break;
+			case '\n':
+				currentLocation.value0 = -1;
+				++currentLocation.value1;
+				break;
+		}
+
+		++currentLocation.value0;
+	}
+
+	std::mt19937 randomNumberGenerator((std::random_device()()));
+
+	std::shuffle(workerTargetLocations.begin(), workerTargetLocations.end(), randomNumberGenerator);
+
+	return grid;
+}

ea56c70d3517c8d4ff287721e9e5b78d44fcf68b
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-05 23:15:59 -0600

    implement directional and addition

--- source_code/vector2.hpp
+++ source_code/vector2.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "direction.hpp"
 #include <array>
 #include <cstdint>
 
@@ -7,15 +8,38 @@ namespace App {
 	template <typename ValueT>
 	class Vector2 {
 		public:
-			ValueT value0{}, value1{};
+			static_assert(std::is_signed_v<ValueT>);
+			using Value = ValueT;
+
+			Value value0{}, value1{};
 
 			[[nodiscard]]
 			constexpr Vector2() = delete;
 
 			[[nodiscard]]
-			constexpr Vector2(ValueT const firstValue, ValueT const secondValue):
+			constexpr Vector2(Value const firstValue, Value const secondValue):
 				value0{firstValue}, value1{secondValue}
 			{}
+
+			[[nodiscard]]
+			Vector2 operator+(Vector2 const &vector) const {
+				return {this->value0 + vector.value0, this->value1 + vector.value1};
+			}
+
+			[[nodiscard]]
+			static constexpr Vector2 getDirection(Direction const direction) {
+				switch (direction) {
+					using enum Direction;
+
+					/**/                              case north: return {-1, 0};
+					/**/
+					/**/ case west: return {0, -1};   case none: return  { 0, 0};   case east: return {0, +1};
+					/**/
+					/**/                              case south: return {+1, 0};
+				}
+
+				throw std::logic_error("Invalid direction.");
+			}
 	};
 
 	using CellGridKey = Vector2<std::int_fast32_t>;

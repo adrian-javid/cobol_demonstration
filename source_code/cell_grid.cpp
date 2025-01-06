76339352d257ab2d963e3df14be591796a181840
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-05 23:16:49 -0600

    implement function `update`

--- source_code/cell_grid.cpp
+++ source_code/cell_grid.cpp
@@ -12,10 +12,37 @@ App::CellGrid::CellGrid(
 void App::CellGrid::update() {
 	for (std::size_t rowIndex{0}; rowIndex < this->rowCount; ++rowIndex) {
 		for (std::size_t columnIndex{0}; columnIndex < this->columnCount; ++columnIndex) {
-			auto const &celllist = (*this)[{rowIndex, columnIndex}];
-			// Cell::Request const &request = cellPointer->getRequest();
+			CellGridKey const currentLocation{
+				static_cast<CellGridKey::Value>(rowIndex),
+				static_cast<CellGridKey::Value>(columnIndex)
+			};
+
+			auto &cellGroup = (*this)[currentLocation];
+
+			for (
+				auto cellPointerIterator = cellGroup.begin();
+				cellPointerIterator != cellGroup.end();
+			) {
+				auto const &cellPointer = *cellPointerIterator;
+				if (cellPointer == nullptr) throw std::logic_error("Null cell pointer.");
+
+				if (cellPointer->getColor() == this->currentCellColor) {
+					Cell::Request const &request = cellPointer->update();
+					
+					if (request.movement != Direction::none) {
+						CellGridKey const targetLocation(currentLocation + CellGridKey::getDirection(request.movement));
+						(*this)[targetLocation].emplace(cellPointer);
+						cellPointerIterator = cellGroup.erase(cellPointerIterator);
+					}
+				} else {
+					++cellPointerIterator;
+				}
+			}
+
 		}
 	}
+
+	this->currentCellColor = Cell::getNextColor(this->currentCellColor);
 }
 
 auto App::CellGrid::operator[](CellGridKey const &key) -> decltype(table)::value_type & {

ae8dff537a386bd092463c476ce4a6275a628c05
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-11 02:03:25 -0600

    use different link to download GMP

--- cobol_to_webassembly/download_cache.mk
+++ cobol_to_webassembly/download_cache.mk
@@ -1,16 +1,9 @@
 
-DOWNLOAD_GMPLIB_SIGNATURE := true
-
-ifeq (${DOWNLOAD_GMPLIB_SIGNATURE},true)
 build/download_cache/${LIB_GMP_TARBALL}: build/download_cache/${LIB_GMP_TARBALL}.sig | build/download_cache/
-	wget --output-document='$@' https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}
+	curl --location --output $@ https://ftp.gnu.org/gnu/gmp/${LIB_GMP_TARBALL}
 
 build/download_cache/${LIB_GMP_TARBALL}.sig: | build/download_cache/
-	wget --output-document='$@' https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}.sig
-else
-build/download_cache/${LIB_GMP_TARBALL}: | build/download_cache/
-	curl --location --output $@ https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}
-endif
+	curl --location --output $@ https://ftp.gnu.org/gnu/gmp/${LIB_GMP_TARBALL}.sig
 
 build/download_cache/${LIB_COB_TARBALL}: build/download_cache/${LIB_COB_TARBALL}.sig | build/download_cache/
 	curl --location --output $@ https://sourceforge.net/projects/gnucobol/files/gnucobol/${LIB_COB_VERSION}/${LIB_COB_TARBALL}/download

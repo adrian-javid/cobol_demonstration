a08d6e1821ecbb365e680103745293df8ed2bda1
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-01-11 01:12:55 -0600

    don't download GMP signature

--- cobol_to_webassembly/download_cache.mk
+++ cobol_to_webassembly/download_cache.mk
@@ -1,9 +1,16 @@
 
+DOWNLOAD_GMPLIB_SIGNATURE := false
+
+ifeq (${DOWNLOAD_GMPLIB_SIGNATURE},true)
 build/download_cache/${LIB_GMP_TARBALL}: build/download_cache/${LIB_GMP_TARBALL}.sig | build/download_cache/
 	curl --location --output $@ https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}
 
 build/download_cache/${LIB_GMP_TARBALL}.sig: | build/download_cache/
 	curl --location --output $@ https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}.sig
+else
+build/download_cache/${LIB_GMP_TARBALL}: | build/download_cache/
+	curl --location --output $@ https://gmplib.org/download/gmp/${LIB_GMP_TARBALL}
+endif
 
 build/download_cache/${LIB_COB_TARBALL}: build/download_cache/${LIB_COB_TARBALL}.sig | build/download_cache/
 	curl --location --output $@ https://sourceforge.net/projects/gnucobol/files/gnucobol/${LIB_COB_VERSION}/${LIB_COB_TARBALL}/download
